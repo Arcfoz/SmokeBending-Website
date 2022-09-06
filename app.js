f39a90fd4b61b5005acbf6075faeb21f31c4fc05
Arcfoz <ikramtauffiqulh@gmail.com>
2022-09-07 03:24:21 +0700

    merubah akun antares

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.get("/dashboard", (req, res) => {
 
 //Restapi
 app.get("/api", function (req, res) {
-  antares.setAccessKey("3b71941b4e3ba492:e952e7b073875cb5");
-  const mq7 = antares.get("Smart-Firebending", "mq7-sensor").then(function (response) {
+  antares.setAccessKey("3c029a06d6d236ae:09deb22e3dd36497");
+  const mq7 = antares.get("Smoke_Bending", "mq7").then(function (response) {
     const data = response.content;
     return data;
   });
 
-  const fan = antares.get("Smart-Firebending", "fan-sensor").then(function (response) {
+  const fan = antares.get("Smoke_Bending", "fan").then(function (response) {
     const data = response.content;
     return data;
   });
@@ -45,8 +45,8 @@ app.post("/api", (req, res) => {
     return res.status(400).send({ status: "failed" });
   }
   res.status(200).send({ status: "recieved" });
-  antares.setAccessKey("3b71941b4e3ba492:e952e7b073875cb5");
-  antares.send(parcel, "Smart-Firebending", "fan-sensor");
+  antares.setAccessKey("3c029a06d6d236ae:09deb22e3dd36497");
+  antares.send(parcel, "Smoke_Bending", "fan");
 });
 
 //Listen
